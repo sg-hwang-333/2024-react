84836d0c08ab45dce8b39928658fd7e95155c364
sg-hwang-333 <w2236@e-mirim.hs.kr>
2024-03-07 09:50:05 +0900

    [FEAT] props 스프레드 연산자

--- src/component-basic/props-basic-1.js
+++ src/component-basic/props-basic-1.js
@@ -4,13 +4,20 @@ const root = ReactDOM.createRoot(document.getElementById("root"))
 // props를 통해서 전달된 값에 접근 가능
 const ComponentWithProps = function(props) {
   console.log(props)
-  return <p>{JSON.stringify(props)}</p>
+  return <p>{JSON.stringify(props.value)}</p>
+}
+
+const Greeting = function(props) {
+  const {age, name} = props
+  return <h1>안녕하세요, {age}살 {name}씨</h1>
 }
 
 root.render(
 <div>
-  <ComponentWithProps value="Hello" />
+  {/* <ComponentWithProps value="Hello" />
   <ComponentWithProps value={1} />
   <ComponentWithProps value={{ a: 1, b: "React" }} />
-  <ComponentWithProps value={() => { }} />
+  <ComponentWithProps value={() => { }} /> */}
+  <Greeting name={"황슬기"} age={19}/>
+  <Greeting name={"홍삼"} age={2}/>
 </div>)
